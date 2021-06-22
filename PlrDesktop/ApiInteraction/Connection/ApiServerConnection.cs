9c9ea16964dc4ec72fefc9e13cb5c14bb8a348f8
Ярослав Грачев <47861604+Ner-Kat@users.noreply.github.com>
2021-06-22 14:48:51 +0300

    Создана система осуществления PlrAPI-корректных запросов с учётом системы аутентификации в API.

---
+++ PlrDesktop/ApiInteraction/Connection/ApiServerConnection.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace PlrDesktop.ApiInteraction.Connection
+{
+    public class ApiServerConnection
+    {
+        private ApiServerInfo _serverInfo;
+        private HttpClient _httpClient;
+        private ApiServerRequester _requester;
+        private AuthProvider _authProvider;
+
+        public AuthInfo AuthInfo
+        {
+            get
+            {
+                return AuthInfo;
+            }
+            set
+            {
+                AuthInfo = value;
+                _authProvider.ChangeInfo(AuthInfo);
+            }
+        }
+
+        public ApiServerConnection(ApiServerInfo serverInfo)
+        {
+            _serverInfo = serverInfo;
+
+            var httpHandler = new HttpClientHandler();
+            _httpClient = new HttpClient(httpHandler);
+
+            _requester = new ApiServerRequester(_httpClient);
+        }
+
+        public ApiServerConnection(ApiServerInfo serverInfo, AuthInfo authInfo) : this(serverInfo)
+        {
+            _authProvider = new AuthProvider(authInfo, _serverInfo.Address + "accounts/", _requester);
+        }
+
+        public async Task<ApiServerRequesterResult> GetAsync(string path)
+        {
+            string fullRequestPath = _serverInfo.Address + path;
+
+            if (_authProvider != null)
+            {
+                return await _authProvider.GetWithAuth(fullRequestPath);
+            }
+
+            return await SimpleGet(fullRequestPath);
+        }
+
+        public async Task<ApiServerRequesterResult> GetWithoutAuthAsync(string path)
+        {
+            string fullRequestPath = _serverInfo.Address + path;
+            return await SimpleGet(fullRequestPath);
+        }
+
+        private async Task<ApiServerRequesterResult> SimpleGet(string address)
+        {
+            var request = _requester.FormNewRequest(HttpMethod.Get, new Uri(address));
+            ApiServerRequesterResult result = await _requester.Send(request);
+            return result;
+        }
+
+        public async Task<ApiServerRequesterResult> PostAsync(string path, object data)
+        {
+            string fullRequestPath = _serverInfo.Address + path;
+
+            if (_authProvider != null)
+            {
+                return await _authProvider.PostWithAuth(fullRequestPath, data);
+            }
+
+            return await SimplePost(fullRequestPath, data);
+        }
+
+        public async Task<ApiServerRequesterResult> PostWithoutAuthAsync(string path, object data)
+        {
+            string fullRequestPath = _serverInfo.Address + path;
+            return await SimplePost(fullRequestPath, data);
+        }
+
+        private async Task<ApiServerRequesterResult> SimplePost(string address, object data)
+        {
+            var request = _requester.FormNewRequest(HttpMethod.Post, new Uri(address));
+
+            string jsonData = JsonSerializer.Serialize(data);
+            _requester.AddData(request, jsonData);
+
+            ApiServerRequesterResult result = await _requester.Send(request);
+            return result;
+        }
+    }
+}
---
+++ PlrDesktop/ApiInteraction/Connection/ApiServerRequesterResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlrDesktop.ApiInteraction.Connection
+{
+    public class ApiServerRequesterResult
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string Content { get; set; }
+        public HttpRequestMessage Request { get; set; }
+    }
+}
